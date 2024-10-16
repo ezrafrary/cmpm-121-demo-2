1c8852b317fb64dcb3b27f6457ec51a8bdd22f94
ezrafrary <91634826+ezrafrary@users.noreply.github.com>
2024-10-16 00:03:15 -0700

    step 1 done

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Ezra's game";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const titleText = document.createElement("h1");
+titleText.innerHTML = `Ezra's Canvas`;
+app.append(titleText);
+
+
+const myCanvas = document.createElement("canvas");
+myCanvas.id = "myCanvas";
+app.append(myCanvas);
+
+
+
